01f60780cf986d6b4fbe28bfa017e0e4c5db686c
danielsickels <drsickels@gmail.com>
2024-02-17 21:46:52 -0500

    fixing bigfivechart rendering

--- src/components/About/BigFiveChart.tsx
+++ src/components/About/BigFiveChart.tsx
@@ -32,10 +32,10 @@ interface BigFiveChartProps {
 
 const BigFiveChart: React.FC<BigFiveChartProps> = ({ data, options }) => {
   return (
-    <div className="flex flex-col items-center justify-center py-10 sm:w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto">
+    <div className="flex flex-col items-center justify-center py-10 ">
       <h2 className="text-5xl font-bold py-10">Who Am I?</h2>
       <h3 className="text-2xl font-bold">My Big Five Traits</h3>
-      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      <div className="flex items-center justify-center w-1/2 h-1/2 ml-12 pl-5 mt-4">
         <PolarArea data={data} options={options} />
       </div>
     </div>
